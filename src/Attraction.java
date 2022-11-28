6e4197ee5f91c5c3e105700bb81bac8a7fea25a7
LeeHyeonji <115696585+GodHyeonG@users.noreply.github.com>
2022-11-28 10:14:14 +0900

    Add files via upload

---
+++ src/Attraction.java
@@ -0,0 +1,49 @@
+public class Attraction {
+    private int x = 0;
+    private int y = 0;
+    private double waiting = 0;
+    private String name = "<NULL>";
+
+    private int visit = 0;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getWaiting() {
+        return waiting;
+    }
+
+    public void setWaiting(double waiting) {
+        this.waiting = waiting;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVisit() {
+        return visit;
+    }
+
+    public void setVisit(int visit) {
+        this.visit = visit;
+    }
+
+}
---
+++ src/Route.java
@@ -0,0 +1,22 @@
+//import static java.lang.constant.ConstantDescs.NULL;
+
+public class Route {
+    private double distance = 0;
+    private boolean possible = false;
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public boolean isPossible() {
+        return possible;
+    }
+
+    public void setPossible(boolean possible) {
+        this.possible = possible;
+    }
+}
